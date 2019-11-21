19cf944c3890ea3b0102c625a0fc05c6a36b4df0
hours_android <pinefirst@outlook.com>
2019-11-22 02:21:42 +0900

    retrofit demo

---
+++ app/src/main/java/com/rim/retrofit/model/SamplePojo.java
@@ -0,0 +1,15 @@
+package com.rim.retrofit.model;
+
+public class SamplePojo {
+
+    private int userId;
+    private String name;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ app/src/main/java/com/rim/retrofit/model/SignUpResponse.java
@@ -0,0 +1,36 @@
+package com.rim.retrofit.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpResponse {
+
+    private String success;
+
+    private String message;
+    private Integer userid;
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(String success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+}
